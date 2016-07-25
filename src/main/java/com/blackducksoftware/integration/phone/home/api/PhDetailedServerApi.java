da54f16105503fd54995da58c97399d5807cfe79
Nicholas Rowles <nrowles@blackducksoftware.com>
2016-07-25 17:07:53 -0400

    Added license info and new REST endpoint

--- src/main/java/com/blackducksoftware/integration/phone/home/api/PhDetailedServerApi.java
+++ src/main/java/com/blackducksoftware/integration/phone/home/api/PhDetailedServerApi.java
@@ -22,24 +22,11 @@
 
 package com.blackducksoftware.integration.phone.home.api;
 
-import org.restlet.Request;
-import org.restlet.Response;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
-import org.restlet.resource.Get;
 import org.restlet.resource.Post;
-import org.restlet.resource.Put;
 
-import com.blackducksoftware.integration.phone.home.api.PhoneHomeInfo;
-
-/**
- * 
- * @author nrowles
- *
- * This interface defines the 'Post' method for the phone-home endpoint
- */
-public interface PhoneHomeServerApi {
-	
+public interface PhDetailedServerApi {
 	@Post("json")
-	public void updatePhoneHomeInfo(JsonRepresentation info);
+	public Representation doPost(JsonRepresentation entity);
 }
--- src/main/java/com/blackducksoftware/integration/phone/home/api/PhoneHomeInfo.java
+++ src/main/java/com/blackducksoftware/integration/phone/home/api/PhoneHomeInfo.java
@@ -1,29 +1,40 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+
 package com.blackducksoftware.integration.phone.home.api;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Serializable;
-import java.io.Writer;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.WritableByteChannel;
-import java.sql.ResultSet;
-import java.util.List;
 import java.util.Properties;
 
-import org.restlet.data.Form;
-import org.restlet.data.MediaType;
+import org.json.JSONObject;
 import org.restlet.ext.json.JsonRepresentation;
-import org.restlet.representation.Representation;
 import org.restlet.resource.ClientResource;
 
-import org.json.JSONObject;
-
 /**
- * 
  * @author nrowles
- *
+ * 
+ * 
+ * 
  */
 public class PhoneHomeInfo{
 	
@@ -36,9 +47,9 @@ public class PhoneHomeInfo{
 	private String targetUrl;
 	
 	//constructors
-	public PhoneHomeInfo(){
-		this.targetUrl = buildUrlFromProperties();
-	};
+//	public PhoneHomeInfo(){
+//		this.targetUrl = buildUrlFromProperties();
+//	};
 	public PhoneHomeInfo(String blackDuckName, 
 			String blackDuckVersion, 
 			String thirdPartyName, 
