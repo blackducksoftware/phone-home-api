2c2097eb691d25291b99ff1dc8d0c71f0de8dc8a
jrichard <jrichard@blackducksoftware.com>
2016-10-12 13:37:18 -0400

    Adding another third party name

--- src/main/java/com/blackducksoftware/integration/phone/home/enums/ThirdPartyName.java
+++ src/main/java/com/blackducksoftware/integration/phone/home/enums/ThirdPartyName.java
@@ -1,21 +1,22 @@
 package com.blackducksoftware.integration.phone.home.enums;
 
 public enum ThirdPartyName {
-	JENKINS ("Jenkins"), 
-	BAMBOO ("Bamboo"), 
-	TEAM_CITY ("Team-City"), 
-	JIRA ("Jira"), 
-	MAVEN ("Maven"), 
-	GRADLE ("Gradle"), 
-	ARTIFACTORY ("Artifactory"), 
-	FORTIFY ("Fortify");
-	
-	private final String name; 
-	
-	private ThirdPartyName(String name){
+	JENKINS ("Jenkins"),
+	BAMBOO ("Bamboo"),
+	TEAM_CITY ("Team-City"),
+	JIRA ("Jira"),
+	MAVEN ("Maven"),
+	GRADLE ("Gradle"),
+	ARTIFACTORY ("Artifactory"),
+	FORTIFY("Fortify"),
+	SONARQUBE("Sonarqube");
+
+	private final String name;
+
+	private ThirdPartyName(final String name){
 		this.name = name;
 	}
-	
+
 	public String getName(){
 		return this.name;
 	}
