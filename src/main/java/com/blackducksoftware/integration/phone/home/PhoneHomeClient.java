70c7e48faba4d710dec33daae3ca1e78fe4ea49e
jrichard <jrichard@blackducksoftware.com>
2017-02-23 14:52:47 -0500

    Updating phone-home to use the new hub-rest-common

--- src/main/java/com/blackducksoftware/integration/phone/home/PhoneHomeClient.java
+++ src/main/java/com/blackducksoftware/integration/phone/home/PhoneHomeClient.java
@@ -22,39 +22,26 @@
 
 package com.blackducksoftware.integration.phone.home;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.digest.MessageDigestAlgorithms;
 import org.apache.commons.lang3.StringUtils;
 
+import com.blackducksoftware.integration.exception.IntegrationException;
+import com.blackducksoftware.integration.hub.request.HubRequest;
+import com.blackducksoftware.integration.hub.request.HubRequestFactory;
+import com.blackducksoftware.integration.hub.rest.RestConnection;
 import com.blackducksoftware.integration.log.IntLogger;
 import com.blackducksoftware.integration.phone.home.enums.BlackDuckName;
 import com.blackducksoftware.integration.phone.home.enums.PhoneHomeSource;
 import com.blackducksoftware.integration.phone.home.enums.ThirdPartyName;
 import com.blackducksoftware.integration.phone.home.exception.PhoneHomeArgumentException;
-import com.blackducksoftware.integration.phone.home.exception.PhoneHomeConnectionException;
 import com.blackducksoftware.integration.phone.home.exception.PhoneHomeException;
-import com.blackducksoftware.integration.phone.home.util.OkAuthenticator;
-import com.blackducksoftware.integration.util.proxy.ProxyUtil;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import okhttp3.HttpUrl;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 /**
  * @author nrowles
@@ -65,10 +52,6 @@ import okhttp3.Response;
  */
 public class PhoneHomeClient {
     private final IntLogger logger;
-
-    private final OkHttpClient.Builder builder = new OkHttpClient.Builder();
-
-    private OkHttpClient client;
 
     private int timeout = 300;
 
@@ -82,44 +65,6 @@ public class PhoneHomeClient {
 
     public void setTimeout(final int timeout) {
         this.timeout = timeout;
-    }
-
-    /**
-     * Clears the previously set System properties I.E. https.proxyHost,
-     * https.proxyPort, http.proxyHost, http.proxyPort, http.nonProxyHosts
-     *
-     */
-    public void setProxyProperties(final String proxyHost, final int proxyPort, final String proxyUser,
-            final String decryptedProxyPassword, final String ignoredProxyHost) throws PhoneHomeArgumentException {
-        if (StringUtils.isNotBlank(proxyHost) && proxyPort > 0) {
-            boolean shouldUseProxy = true;
-            if (StringUtils.isNotBlank(ignoredProxyHost)) {
-                final PropertiesLoader propertiesLoader = new PropertiesLoader(logger);
-                URL targetUrl;
-                try {
-                    targetUrl = new URL(propertiesLoader.createTargetUrl(PhoneHomeApiConstants.PROPERTIES_FILE_NAME));
-                } catch (final Exception e) {
-                    throw new PhoneHomeArgumentException(e.getMessage(), e);
-                }
-
-                final List<Pattern> ignoredProxyHostPatterns = ProxyUtil.getIgnoredProxyHostPatterns(ignoredProxyHost);
-                shouldUseProxy = !ProxyUtil.shouldIgnoreHost(targetUrl.getHost(), ignoredProxyHostPatterns);
-            }
-            if (shouldUseProxy) {
-                final Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
-                builder.proxy(proxy);
-                if (StringUtils.isNotBlank(proxyUser) && StringUtils.isNotBlank(decryptedProxyPassword)) {
-                    builder.proxyAuthenticator(new OkAuthenticator(proxyUser, decryptedProxyPassword));
-                }
-            }
-        }
-    }
-
-    private void createClient() {
-        builder.connectTimeout(timeout, TimeUnit.SECONDS);
-        builder.writeTimeout(timeout, TimeUnit.SECONDS);
-        builder.readTimeout(timeout, TimeUnit.SECONDS);
-        client = builder.build();
     }
 
     /**
@@ -139,30 +84,29 @@ public class PhoneHomeClient {
         if (StringUtils.isBlank(targetUrl)) {
             throw new PhoneHomeArgumentException("The targetURL for the phone home was not provided.");
         }
-        createClient();
-        final HttpUrl httpUrl = HttpUrl.parse(targetUrl).newBuilder().build();
-
-        final Gson gson = new GsonBuilder().create();
-        final String json = gson.toJson(info);
-
-        final RequestBody body = RequestBody.create(MediaType.parse("application/json"), json);
-
-        final Request request = new Request.Builder().url(httpUrl).post(body).build();
-
-        Response response = null;
+        URL url;
         try {
-            try {
-                response = client.newCall(request).execute();
-            } catch (final IOException e) {
-                throw new PhoneHomeConnectionException(e.getMessage(), e);
+            url = new URL(targetUrl);
+        } catch (final MalformedURLException e) {
+            throw new PhoneHomeException(e.getMessage(), e);
+        }
+        final RestConnection restConnection = new RestConnection(logger, url, timeout) {
+            @Override
+            public void clientAuthenticate() throws IntegrationException {
             }
-            if (!response.isSuccessful()) {
-                throw new PhoneHomeConnectionException(response.message());
+
+            @Override
+            public void addBuilderAuthentication() throws IntegrationException {
             }
-        } finally {
-            if (response != null) {
-                response.close();
-            }
+        };
+
+        final HubRequestFactory factory = new HubRequestFactory(restConnection);
+        final HubRequest request = factory.createRequest(targetUrl);
+
+        try {
+            request.executePost(restConnection.getGson().toJson(info));
+        } catch (final IntegrationException e) {
+            throw new PhoneHomeException(e.getMessage(), e);
         }
     }
 
